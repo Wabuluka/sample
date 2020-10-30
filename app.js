a7223b237cd68b7476d95cb94766561c2a598515
unknown <dwabuluka@gmail.com>
2020-10-30 04:40:48 +0300

    testing testing

---
+++ app.js
@@ -0,0 +1,17 @@
+
+class App {
+    static add(a, b) {
+      return a + b;
+    }
+    static substract(a, b) {
+      return a - b;
+    }
+    static multiply(a, b) {
+      return a * b;
+    }
+    static divide(a, b) {
+      return a / b;
+    }
+  }
+  
+module.exports = App;
