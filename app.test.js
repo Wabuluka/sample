0ae6008513d992429cf90cb1e94b45c6610f9c21
Wabuluka <dwabuluka@gmail.com>
2020-11-03 09:11:56 +0300

    another testing success

--- app.test.js
+++ app.test.js
@@ -1,9 +1,14 @@
 const chai = require('chai');
 const app = require('./app');
 const chaiHttp = require('chai-http');
+const contacts = require('./data/data')
+
 chai.should();
 chai.use(chaiHttp);
 
+
+
+const contact = contacts.contacts
 
 describe('Testing the default routes', () =>{
   it('Should check the landing url', (done) =>{
@@ -15,4 +20,44 @@ describe('Testing the default routes', () =>{
               done();
           });
   });
+
+  it('Should add a contact to the list', (done) => {
+    chai
+        .request(app)
+        .post('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+  })
+
+  it('Should get a contact from the list', (done) => {
+    chai
+        .request(app)
+        .get('/1')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+  })
+
+  it('Should edit a contact to the list', (done) => {
+    chai
+        .request(app)
+        .put('/1')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+  })
+
+  it('Should edit a contact to the list', (done) => {
+    chai
+        .request(app)
+        .delete('/1')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+  })
 })
